88b51f58872b8d5d7c815f83c60ee711fab5d6aa
Snikimonkd <cepminkis132@gmail.com>
2025-02-03 10:02:10 +0300

    fix map memory

--- src/font/frag.glsl
+++ src/font/frag.glsl
@@ -6,10 +6,19 @@ uniform vec3 out_color;
 in vec2 out_uv;
 out vec4 color;
 
+float cubicPulse( float c, float w, float x )
+{
+    x = abs(x - c);
+    if( x>w ) return 0.0;
+    x /= w;
+    return 1.0 - x*x*(3.0-2.0*x);
+}
+
 void main()
 {
     float d = texture(image, out_uv).r;
     float aaf = fwidth(d);
     float alpha = smoothstep(0.5 - aaf, 0.5 + aaf, d);
+//    float alpha = cubicPulse(0.5, aaf, d);
     color = vec4(out_color, alpha);
 }
